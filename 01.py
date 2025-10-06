56a0bc7e61140d0402c225012a7c2ebc19fc4e68
maria fernanda <nandadias1411@gmail.com>
2025-10-06 15:21:02 -0300

    Update 01.py

--- 01.py
+++ 01.py
@@ -1,3 +1,8 @@
 import streamlit as st
-st.image("minha_imagem.jpg", caption="Pôr do Sol nas montanhas", width=500)
+st.title("esse é o meu progama")
+
 st.write("Oie mundo")
+
+nome = st.text_input("Poderia digitar seu nome, por favor?!")
+if nome:
+  st.writte(nome.upper())
